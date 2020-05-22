18d9faf19340f1810bb7ad0dad536d286c6f8284
eto-shingo <=>
2020-05-22 17:58:50 +0900

    hashmap to struct

--- omoinas/src/model.rs
+++ omoinas/src/model.rs
@@ -1,4 +1,5 @@
 pub mod mono;
+pub mod setting;
 
 use std::cmp::PartialEq;
 
--- omoinas/src/repository/cache.rs
+++ omoinas/src/repository/cache.rs
@@ -1,21 +1,11 @@
 use log::{debug, error};
-use std::collections::HashMap;
-use std::default::Default;
-use std::env;
 
 use chrono::{FixedOffset, Utc};
 use rusoto_core::Region;
-use rusoto_dynamodb::{AttributeValue, DynamoDb, DynamoDbClient, GetItemInput, PutItemInput};
+use rusoto_dynamodb::{DynamoDb, DynamoDbClient};
 
 use crate::cotoha;
-
-fn key_cotoha_token() -> String {
-    return String::from("cotoha_token");
-}
-
-fn table_setting() -> String {
-    return format!("{}_{}", env::var("ENV").unwrap(), "setting");
-}
+use crate::model::setting::CotohaToken;
 
 pub fn get_cotoha_token() -> Option<String> {
     let today = Utc::now()
@@ -23,27 +13,15 @@ pub fn get_cotoha_token() -> Option<String> {
         .format("%Y%m%d")
         .to_string();
     let client = DynamoDbClient::new(Region::ApNortheast1);
-    let mut key: HashMap<String, AttributeValue> = HashMap::new();
-    key.insert(
-        String::from("name"),
-        AttributeValue {
-            s: Some(key_cotoha_token()),
-            ..Default::default()
-        },
-    );
-    let get_item = GetItemInput {
-        key: key,
-        table_name: table_setting(),
-        ..Default::default()
-    };
-
-    match client.get_item(get_item).sync() {
+    match client.get_item(CotohaToken::get_item()).sync() {
         Ok(result) => match result.item {
             Some(item) => {
-                if item.get("date").is_some() && item["date"].s.is_some() {
-                    return item["token"].s.clone();
+                if let Some(ct) = CotohaToken::from(item) {
+                    if ct.date() == today {
+                        return Some(ct.token());
+                    }
+                    debug!("expired cotoha token.");
                 }
-                debug!("expired cotoha token.");
             }
             None => {
                 debug!("no cotoha token.()");
@@ -51,45 +29,16 @@ pub fn get_cotoha_token() -> Option<String> {
         },
         Err(err) => {
             error!("{}", err);
-            return None;
         }
     };
 
     debug!("get_cotoha_token()");
     match cotoha::api::get_access_token() {
         Ok(t) => {
-            let mut item: HashMap<String, AttributeValue> = HashMap::new();
-            item.insert(
-                String::from("name"),
-                AttributeValue {
-                    s: Some(key_cotoha_token()),
-                    ..Default::default()
-                },
-            );
-            item.insert(
-                String::from("date"),
-                AttributeValue {
-                    s: Some(today),
-                    ..Default::default()
-                },
-            );
-            item.insert(
-                String::from("token"),
-                AttributeValue {
-                    s: Some(t.clone()),
-                    ..Default::default()
-                },
-            );
-            let put_item = PutItemInput {
-                item: item,
-                table_name: table_setting(),
-                ..Default::default()
-            };
-            match client.put_item(put_item).sync() {
+            match client.put_item(CotohaToken::new(&t).put_item()).sync() {
                 Ok(_) => {}
                 Err(err) => error!("{}", err),
             }
-
             return Some(t);
         }
         Err(err) => {
